300ea58a4b7071f0d8e5a0ebb0b28c2a5ac2683c
Bogdan Popa <bogdan@defn.io>
2021-12-05 11:23:36 +0200

    optimize day 5 for time

--- day05.rkt
+++ day05.rkt
@@ -1,27 +1,28 @@
 #lang racket/base
 
-(require racket/match)
+(require racket/fixnum
+         racket/match)
 
 (struct line (x1 y1 x2 y2)
   #:transparent)
 
-(define (sign n)
+(define (fxsign n)
   (cond
-    [(= n 0) 0]
-    [(> n 0) 1]
-    [(< n 0) -1]))
+    [(fx= n 0) 0]
+    [(fx> n 0) 1]
+    [(fx< n 0) -1]))
 
 ;; in 1d space
 (define (line-points l M [ok? (λ (_dx _dy) #t)])
   (match-define (line x1 y1 x2 y2) l)
-  (define dx (sign (- x2 x1)))
-  (define dy (sign (- y2 y1)))
+  (define dx (fxsign (- x2 x1)))
+  (define dy (fxsign (- y2 y1)))
   (if (ok? dx dy)
-      (for/list ([i (in-inclusive-range 0 (max (abs (- x2 x1))
-                                               (abs (- y2 y1))))])
-        (define x (+ x1 (* i dx)))
-        (define y (+ y1 (* i dy)))
-        (+ x (* y M)))
+      (for/list ([i (in-inclusive-range 0 (fxmax (fxabs (fx- x2 x1))
+                                                 (fxabs (fx- y2 y1))))])
+        (define x (fx+ x1 (fx* i dx)))
+        (define y (fx+ y1 (fx* i dy)))
+        (fx+ x (fx* y M)))
       null))
 
 (define-values (M lines)
@@ -43,16 +44,14 @@
             (cons (line x1 y1 x2 y2) lines))])))))
 
 (define (solution points-proc)
-  (define (count-dangerous points)
-    (for/sum ([cnt (in-hash-values points)] #:when (> cnt 1)) 1))
-  (for*/fold ([points (make-hasheqv)] #:result (count-dangerous points))
-             ([l (in-list lines)]
-              [p (in-list (points-proc l))])
-    (begin0 points
-      (hash-update! points p add1 0))))
+  (define points (make-fxvector (+ M (* M M)) 0))
+  (for* ([l (in-list lines)]
+         [p (in-list (points-proc l))])
+    (fxvector-set! points p (fx+ 1 (fxvector-ref points p))))
+  (for/sum ([n (in-fxvector points)] #:when (fx> n 1)) 1))
 
-(define part1 (solution (λ (l) (line-points l M (λ (dx dy) (or (zero? dx) (zero? dy)))))))
-(define part2 (solution (λ (l) (line-points l M))))
+(define part1 (time (solution (λ (l) (line-points l M (λ (dx dy) (or (fx= dx 0) (fx= dy 0))))))))
+(define part2 (time (solution (λ (l) (line-points l M)))))
 
 (module+ test
   (require rackunit)
