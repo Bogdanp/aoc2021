6d020aa7eb6b93802bb21d2bd4ab3dab55c3cca4
Bogdan Popa <bogdan@defn.io>
2021-12-08 12:56:16 +0200

    day08: use racket/set

--- day08.rkt
+++ day08.rkt
@@ -2,6 +2,7 @@
 
 (require racket/list
          racket/match
+         racket/set
          racket/string)
 
 (define data
@@ -11,52 +12,38 @@
         (match (string-split line " | ")
           [(list input output)
            (list
-            (map string->list (string-split input))
-            (map string->list (string-split output)))])))))
-
-(define (simple-digit cs)
-  (case (length cs)
-    [(2) 1]
-    [(4) 4]
-    [(3) 7]
-    [(7) 8]
-    [else #f]))
-
-(define (charset cs)
-  (sort (remove-duplicates cs) char<?))
-
-(define (charset-overlap? as bs)
-  (for/and ([a (in-list as)])
-    (memv a bs)))
+            (map (compose1 list->seteqv string->list) (string-split input))
+            (map (compose1 list->seteqv string->list) (string-split output)))])))))
 
 (define (deduce-mapping css)
-  (define m1 (for/first ([cs (in-list css)] #:when (= (length cs) 2)) cs))
-  (define m4 (for/first ([cs (in-list css)] #:when (= (length cs) 4)) cs))
-  (define m7 (for/first ([cs (in-list css)] #:when (= (length cs) 3)) cs))
-  (define m8 (for/first ([cs (in-list css)] #:when (= (length cs) 7)) cs))
-  (define m9 (for/first ([cs (in-list css)] #:when (and (= (length cs) 6) (charset-overlap? m4 cs))) cs))
-  (define m0 (for/first ([cs (in-list css)] #:when (and (= (length cs) 6) (not (member cs (list m9))) (charset-overlap? m1 cs))) cs))
-  (define m6 (for/first ([cs (in-list css)] #:when (and (= (length cs) 6) (not (member cs (list m0 m9))))) cs))
-  (define m3 (for/first ([cs (in-list css)] #:when (and (= (length cs) 5) (charset-overlap? m1 cs))) cs))
-  (define m5 (for/first ([cs (in-list css)] #:when (and (= (length cs) 5) (not (member cs (list m3))) (charset-overlap? cs m9))) cs))
-  (define m2 (for/first ([cs (in-list css)] #:when (and (= (length cs) 5) (not (member cs (list m3 m5))))) cs))
+  (define (first-where valid?-proc)
+    (for/first ([cs (in-list css)] #:when (valid?-proc cs)) cs))
+  (define m1 (first-where (λ (cs) (= (set-count cs) 2))))
+  (define m4 (first-where (λ (cs) (= (set-count cs) 4))))
+  (define m7 (first-where (λ (cs) (= (set-count cs) 3))))
+  (define m8 (first-where (λ (cs) (= (set-count cs) 7))))
+  (define m9 (first-where (λ (cs) (and (= (set-count cs) 6) (subset? m4 cs)))))
+  (define m0 (first-where (λ (cs) (and (= (set-count cs) 6) (not (member cs (list m9))) (subset? m1 cs)))))
+  (define m6 (first-where (λ (cs) (and (= (set-count cs) 6) (not (member cs (list m0 m9)))))))
+  (define m3 (first-where (λ (cs) (and (= (set-count cs) 5) (subset? m1 cs)))))
+  (define m5 (first-where (λ (cs) (and (= (set-count cs) 5) (not (member cs (list m3))) (subset? cs m9)))))
+  (define m2 (first-where (λ (cs) (and (= (set-count cs) 5) (not (member cs (list m3 m5)))))))
   (list m0 m1 m2 m3 m4 m5 m6 m7 m8 m9))
 
 (define part1
   (time
    (for*/sum ([d (in-list data)]
               [cs (in-list (cadr d))]
-              #:when (simple-digit cs))
+              #:when (memv (set-count cs) '(2 4 3 7)))
      1)))
 
 (define part2
   (time
    (for/sum ([d (in-list data)])
-     (define css (map charset (car d)))
-     (define mapping (deduce-mapping css))
+     (define mapping (deduce-mapping (car d)))
      (define digits
        (for/list ([cs (in-list (cadr d))])
-         (index-of mapping (charset cs))))
+         (index-of mapping cs)))
      (define number
        (for/sum ([d (in-list digits)]
                  [e (in-range (sub1 (length digits)) -1 -1)])
