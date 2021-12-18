bbc96dae2aa6226a810edcad59c071a4397b6171
Bogdan Popa <bogdan@defn.io>
2021-12-18 12:07:08 +0200

    clean up day18

--- day18.rkt
+++ day18.rkt
@@ -18,20 +18,15 @@
      (write-num (cdr n))
      (display #\])]))
 
-(define numbers
+(define nums
   (call-with-input-file "day18.txt"
     (lambda (in)
-      (for/list ([line (in-lines in)]) (read-num line)))))
+      (for/list ([line (in-lines in)])
+        (read-num line)))))
 
 (struct node (parent left right) #:mutable #:transparent)
 (struct pair node (a b) #:mutable #:transparent)
 (struct num node (n) #:mutable #:transparent)
-
-(define (node->pair n)
-  (cond
-    [(num? n) (num-n n)]
-    [else (cons (node->pair (pair-a n))
-                (node->pair (pair-b n)))]))
 
 (define (node-left* n)
   (when (procedure? (node-left n))
@@ -43,103 +38,108 @@
     (set-node-right! n ((node-right n))))
   (node-right n))
 
-(define ((make-adder accessor) m v)
+(define (node->num m)
+  (if (num? m)
+      (num-n m)
+      (cons (node->num (pair-a m))
+            (node->num (pair-b m)))))
+
+(define (num->node n)
+  (let loop ([parent #f] [n n])
+    (cond
+      [(number? n)
+       (num parent #f #f n)]
+      [else
+       (define m (pair parent #f #f #f #f))
+       (define a (loop m (car n)))
+       (define b (loop m (cdr n)))
+       (set-pair-a! m a)
+       (set-pair-b! m b)
+       (set-node-left!  b a)
+       (set-node-right! a b)
+       (set-node-left!  a (λ () (node-left*  m)))
+       (set-node-right! b (λ () (node-right* m)))
+       m])))
+
+(define ((make-pair-adder accessor) m v)
   (let loop ([m m])
     (if (num? m)
         (set-num-n! m (+ (num-n m) v))
         (loop (accessor m)))))
 
-(define add-left! (make-adder pair-a))
-(define add-right! (make-adder pair-b))
+(define pair-add-left!  (make-pair-adder pair-a))
+(define pair-add-right! (make-pair-adder pair-b))
 
-(define (step n)
-  (define m
-    (let help ([parent #f] [n n])
-      (cond
-        [(number? n)
-         (num parent #f #f n)]
-        [else
-         (define m (pair parent #f #f #f #f))
-         (define a (help m (car n)))
-         (define b (help m (cdr n)))
-         (set-pair-a! m a)
-         (set-pair-b! m b)
-         (set-node-left! a (λ () (node-left* m)))
-         (set-node-right! a b)
-         (set-node-left! b a)
-         (set-node-right! b (λ () (node-right* m)))
-         m])))
-  (define exploded?
-    (let loop ([m m] [depth 0])
-      (cond
-        [(num? m) #f]
-        [(= depth 4)
-         (define p (node-parent m))
-         (define a (pair-a m))
-         (define b (pair-b m))
-         (define l (node-left* m))
-         (define r (node-right* m))
-         (begin0 #t
-           (when l (add-right! l (num-n a)))
-           (when r (add-left! r (num-n b)))
-           (if (eq? (pair-a p) m)
-               (set-pair-a! p (num p (node-left* a) (node-right* a) 0))
-               (set-pair-b! p (num p (node-left* b) (node-right* b) 0))))]
-        [else
-         (or (loop (pair-a m) (add1 depth))
-             (loop (pair-b m) (add1 depth)))])))
-  (define split?
-    (and (not exploded?)
-         (let loop ([m m])
-           (cond
-             [(and (num? m)
-                   (> (num-n m) 9))
-              (define p (node-parent m))
-              (define new-m (pair p (node-left* m) (node-right* m) #f #f))
-              (define num-a (num p #f #f (exact-floor (/ (num-n m) 2))))
-              (define num-b (num p #f #f (exact-ceiling (/ (num-n m) 2))))
-              (set-pair-a! new-m num-a)
-              (set-pair-b! new-m num-b)
-              (set-node-left! num-a (λ () (node-left* m)))
-              (set-node-right! num-a num-b)
-              (set-node-left! num-b num-a)
-              (set-node-right! num-b (λ () (node-right* m)))
-              (begin0 #t
-                (if (eq? (pair-a p) m)
-                    (set-pair-a! p new-m)
-                    (set-pair-b! p new-m)))]
-             [(num? m) #f]
-             [else
-              (or (loop (pair-a m))
-                  (loop (pair-b m)))]))))
-  (values (or exploded? split?) (node->pair m)))
+(define (pair-replace! p old-m new-m)
+  (if (eq? (pair-a p) old-m)
+      (set-pair-a! p new-m)
+      (set-pair-b! p new-m)))
 
-(define add-nums cons)
-(define (reduce-num n)
+(define (try-explode! m)
+  (let loop ([m m] [depth 0])
+    (cond
+      [(num? m) #f]
+      [(= depth 4)
+       (define p (node-parent m))
+       (define a (pair-a m))
+       (define b (pair-b m))
+       (cond [(node-left*  m) => (λ (l) (pair-add-right! l (num-n a)))])
+       (cond [(node-right* m) => (λ (r) (pair-add-left!  r (num-n b)))])
+       (pair-replace! p m (num p (node-left* a) (node-right* a) 0))
+       #t]
+      [else
+       (or (loop (pair-a m) (add1 depth))
+           (loop (pair-b m) (add1 depth)))])))
+
+(define (try-split! m)
+  (let loop ([m m])
+    (if (num? m)
+        (cond
+          [(> (num-n m) 9)
+           (define p (node-parent m))
+           (define num-a (num p (node-left* m) #f (exact-floor (/ (num-n m) 2))))
+           (define num-b (num p #f (node-right* m) (exact-ceiling (/ (num-n m) 2))))
+           (define new-m (pair p (node-left* m) (node-right* m) num-a num-b))
+           (set-node-left!  num-b num-a)
+           (set-node-right! num-a num-b)
+           (pair-replace! p m new-m)
+           #t]
+          [else #f])
+        (or (loop (pair-a m))
+            (loop (pair-b m))))))
+
+(define (num-step n)
+  (define m (num->node n))
+  (define exploded? (try-explode! m))
+  (define split? (and (not exploded?) (try-split! m)))
+  (values (or exploded? split?) (node->num m)))
+
+(define num-add cons)
+(define (num-reduce n)
   (define-values (reduced? new-n)
-    (step n))
-  (if reduced? (reduce-num new-n) n))
+    (num-step n))
+  (if reduced? (num-reduce new-n) n))
 
-(define (magnitude n)
+(define (num-magnitude n)
   (if (number? n)
       n
-      (+ (* 3 (magnitude (car n)))
-         (* 2 (magnitude (cdr n))))))
+      (+ (* 3 (num-magnitude (car n)))
+         (* 2 (num-magnitude (cdr n))))))
 
 (define part1
   (time
-   (magnitude
-    (for/fold ([sum (car numbers)])
-              ([n (in-list (cdr numbers))])
-      (reduce-num (add-nums sum n))))))
+   (num-magnitude
+    (for/fold ([sum (car nums)])
+              ([n (in-list (cdr nums))])
+      (num-reduce (num-add sum n))))))
 
 (define part2
   (time
    (for*/fold ([largest 0])
-              ([a (in-list numbers)]
-               [b (in-list numbers)]
+              ([a (in-list nums)]
+               [b (in-list nums)]
                #:unless (eq? a b))
-     (define m (magnitude (reduce-num (add-nums a b))))
+     (define m (num-magnitude (num-reduce (num-add a b))))
      (if (> m largest) m largest))))
 
 (module+ test
